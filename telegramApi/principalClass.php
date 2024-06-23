b3d125567be24c6c2ac59dab07784391b9572cb1
Cristiansajksa <155487691+Cristiansajksa@users.noreply.github.com>
2024-06-23 17:38:18 -0400

    Update principalClass.php

--- telegramApi/principalClass.php
+++ telegramApi/principalClass.php
@@ -17,19 +17,25 @@ class TelegramApi
     }
     
     
-    public function sendM(int $chatId, string $msg, int $msgReplit = 0, string $buttonsJson = "") : self
+
+    public function sendM(
+        int $chatId, string $msg, int $msgReplit = 0, ?string $buttonsJson = "", bool $editMsg = true
+    ) : self
     {
         $objectCurl = $this->objectManegeApiHttps->Get( "sendMessage", "chat_id=$chatId&text=" . urlencode($msg) . "&reply_to_message_id=$msgReplit&parse_mode=HTML&reply_markup=$buttonsJson" );
-        $this->msgId = $this->objectManegeApiHttps->executeRetrys( $objectCurl )->result->message_id;
+        $keepDates = $this->objectManegeApiHttps->executeRetrys( $objectCurl );
+        $this->msgId = $editMsg ? $keepDates->result->message_id : 0;
+        
         curl_close( $objectCurl );
         return $this;
     }
 
 
+
     public function downloadFile(string $fileId, string $nameFileForKeep) : void
     {
         $objectCurl = $this->objectManegeApiHttps->Get( "getFile", "file_id=$fileId" );
-        $jsonObject = $this->objectManegeApiHttps->executeRetrys ($objectCurl );
+        $jsonObject = $this->objectManegeApiHttps->executeRetrys( $objectCurl );
         curl_close( $objectCurl );
         
         file_put_contents( 
@@ -40,10 +46,66 @@ class TelegramApi
 
 
 
-    public function editM(int $chatId, string $msg, int $msgReplit = 0, string $buttonsJson = "") : self
+    public function editM(
+        int $chatId, string $msg, int $msgId = null, string $buttonsJson = ""
+    ) : self
     {
-        $objectCurl = $this->objectManegeApiHttps->Get( "editMessageText", "chat_id=$chatId&text=" . urlencode($msg) . "&message_id={$this->msgId}&parse_mode=HTML&reply_markup=$buttonsJson" );
+        $msgId = $msgId ?? $this->msgId;
+        $objectCurl = $this->objectManegeApiHttps->Get( "editMessageText", "chat_id=$chatId&text=" . urlencode($msg) . "&message_id=$msgId&parse_mode=HTML&reply_markup=$buttonsJson" );
+        $this->objectManegeApiHttps->executeRetrys( $objectCurl );
+
         curl_close( $objectCurl );
         return $this;
     }
+
+
+
+    public function alertButton(int $buttonId, string $msg) : self
+    {
+        $objectCurl = $this->objectManegeApiHttps->Get( "answerCallbackQuery", "callback_query_id=$buttonId&text=" . urlencode($msg) . "&show_alert=1" );
+        $this->objectManegeApiHttps->executeRetrys( $objectCurl );
+        curl_close( $objectCurl );
+        return $this;
+    }
+
+
+
+    public function sendD(
+        int $chatId, string $msg, string $nameFile, string $jsonButtons = ""
+    ) : self
+    {
+        $postFieldSend = [
+            'document' => curl_file_create($nameFile),
+            'caption' => $msg,
+            'reply_markup' => $jsonButtons,
+            "chat_id" => $chatId,
+            "parse_mode" => "HTML",
+        ];
+
+        $objectCurl = $this->objectManegeApiHttps->post( "sendDocument", $postFieldSend );
+        $this->objectManegeApiHttps->executeRetrys( $objectCurl );
+        curl_close( $objectCurl );
+        return $this;
+    }
+
+
+
+    public function KickMember(int $chatId, int $userId) : self
+    {
+        $objectCurl = $this->objectManegeApiHttps->Get( "kickChatMember", "chat_id=$chatId&user_id=$userId" );
+        $this->objectManegeApiHttps->executeRetrys( $objectCurl );
+        curl_close( $objectCurl );
+        return $this;
+    }
+
+
+
+    public function UnbanMember(int $chatId, int $userId) : self
+    {
+        $objectCurl = $this->objectManegeApiHttps->Get( "unbanChatMember", "chat_id=$chatId&user_id=$userId" );
+        $this->objectManegeApiHttps->executeRetrys( $objectCurl );
+        curl_close( $objectCurl );
+        return $this;
+    }
+    
 }
