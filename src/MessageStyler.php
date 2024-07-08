ec647060c161257913be334a0b17ed8f7877e384
Cristiansajksa <155487691+Cristiansajksa@users.noreply.github.com>
2024-07-08 03:41:19 -0400

    Update MessageStyler.php

--- src/MessageStyler.php
+++ src/MessageStyler.php
@@ -1,5 +1,5 @@
 <?php
-class MessageStyler implements StylerMsgInterface 
+class StylerMsg implements StylerMsgInterface 
 {
     public string $keepButtons = "";
     public string $msgForParse, $msgResult;
@@ -7,28 +7,29 @@ class MessageStyler implements StylerMsgInterface
 
     public function __construct(string $msgForParse) 
     {
-        $this->msgResult = $this->msgForParse = $msgForParse;
+        $this->msgForParse = $msgForParse;
     }
 
 
 
     private function CheckingButtons(array $keepButtons) : string
     {
-        foreach ($keepButtons as &$stringButtons) {
-            $stringButtons = str_replace( ["(", ")"], "", $stringButtons );
-            for ($offSetCount = 0; 2 > $offSetCount; $offSetCount++) {
-                $divideInfo = explode( "||", $stringButtons );
+        foreach ($keepButtons as &$buttonsIterable) {
+            $buttonsIterable = str_replace( ["(", ")"], "", $buttonsIterable );
 
-                if (stristr($divideInfo[$offSetCount], "http")) {
+            for ($offSetCount = 0; 2 > $offSetCount; $offSetCount++) {
+                $divideInfo = explode( "||", $buttonsIterable );
+
+                if (!parse_url($divideInfo[$offSetCount])["host"]) {
                     $nameButton = $offSetCount == 0 ? $divideInfo[1] : $divideInfo[0];
-                    $keepInfo = ["text" => $nameButton, "url" => $divideInfo[$offSetCount]];
+                    $buttonsIterable = ["text" => $nameButton, "url" => $divideInfo[$offSetCount]];
                     break;
                 }
 
-                $keepInfo = ["text" => $divideInfo[0], "callback_data" => $divideInfo[1]];
+                $buttonsIterable = ["text" => $divideInfo[0], "callback_data" => $divideInfo[1]];
             }
-            $stringButtons = $keepInfo;
         }
+
         return json_encode( ["inline_keyboard" => array_chunk($keepButtons, 2)] );
     }
 
@@ -37,7 +38,7 @@ class MessageStyler implements StylerMsgInterface
     public function ParseButtons() : self
     {
         if (preg_match_all("#\\([\S]+\\|\\|[\S]+\\)#", $this->msgForParse, $matchButtons)) {
-            $this->msgResult = $this->msgForParse = str_replace( $matchButtons[0], "", $this->msgForParse );
+            $this->msgForParse = str_replace( $matchButtons[0], "", $this->msgForParse );
             $this->keepButtons = $this->CheckingButtons( $matchButtons[0] );
         }
         return $this;
@@ -47,9 +48,9 @@ class MessageStyler implements StylerMsgInterface
 
     public function ProcessVar() : self 
     {
+        $this->msgResult = $this->msgForParse;
+        
         if (preg_match_all("#\\$[A-Z]\w+#i", $this->msgForParse, $matchVarsLiterally)) {
-            $this->msgResult = $this->msgForParse;
-
             foreach ($matchVarsLiterally[0] as $varsLiterally) {
                 $nameVars = str_replace( "\$", "", $varsLiterally );
                 $valueReplace = isset( $GLOBALS[$nameVars] ) ? $GLOBALS[$nameVars] : "";
