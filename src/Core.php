2e1afa5cb159ca26bd6905faf00efffbb8660b12
Cristiansajksa <155487691+Cristiansajksa@users.noreply.github.com>
2024-10-01 19:38:25 -0400

    Update Core.php

--- src/Core.php
+++ src/Core.php
@@ -1,19 +1,35 @@
 <?php
-class Core
+class TelegramApi
 {
     private object $objectManegeApiHttps;
     private string $botToken;
     private array $keepParams;
+
     private int $msgId = 0;
+    public static $objectTelegramApi;
 
 
-    public function __construct(string $botToken, bool $verifyToken = true) 
+    private function __construct(string $botToken, bool $verifyToken = true) 
     {
-        $this->objectManegeApiHttps = new Api( $botToken );
+        self::$objectTelegramApi = $this;
+        $this->objectManegeApiHttps = new ManegeApiHttps( $botToken );
         $this->botToken = $botToken;
+
         if ($verifyToken) {
             $this->objectManegeApiHttps->CheckerResult( $this->objectManegeApiHttps->Get("getme"), "getme" );
         }
+    }
+
+
+
+    public static function getObject(mixed ...$keepValues) : object 
+    {
+        $objectSend = match (empty(self::$objectTelegramApi)) {
+            true => new self(...$keepValues);
+            false => self::$objectTelegramApi
+        };
+
+        return $objectSend;
     }
     
     
@@ -24,6 +40,7 @@ class Core
     {
         $objectCurl = $this->objectManegeApiHttps->Get( "sendMessage", "chat_id=$chatId&text=" . urlencode($msg) . "&reply_to_message_id=$msgReplit&parse_mode=HTML&reply_markup=$buttonsJson" );
         $keepDates = $this->objectManegeApiHttps->executeRetrys( $objectCurl );
+
         $this->msgId = $editMsg ? $keepDates->result->message_id : 0;
         
         curl_close( $objectCurl );
@@ -38,19 +55,16 @@ class Core
         $jsonObject = $this->objectManegeApiHttps->executeRetrys( $objectCurl );
         curl_close( $objectCurl );
         
-        file_put_contents( 
-            $nameFileForKeep, 
-            file_get_contents("https://api.telegram.org/file/bot$this->botToken/{$jsonObject->result->file_path}")
-        );
+        $keepFile = file_get_contents( "https://api.telegram.org/file/bot$this->botToken/{$jsonObject->result->file_path}" );
+        file_put_contents( $nameFileForKeep, $keepFile );
     }
 
 
 
-    public function editM(
-        int $chatId, string $msg, int $msgId = null, string $buttonsJson = ""
-    ) : self
+    public function editM(int $chatId, string $msg, int $msgId = null, string $buttonsJson = "") : self
     {
         $msgId = $msgId ?? $this->msgId;
+
         $objectCurl = $this->objectManegeApiHttps->Get( "editMessageText", "chat_id=$chatId&text=" . urlencode($msg) . "&message_id=$msgId&parse_mode=HTML&reply_markup=$buttonsJson" );
         $this->objectManegeApiHttps->executeRetrys( $objectCurl );
 
@@ -70,9 +84,7 @@ class Core
 
 
 
-    public function sendD(
-        int $chatId, string $msg, string $nameFile, string $jsonButtons = ""
-    ) : self
+    public function sendD(int $chatId, string $msg, string $nameFile, string $jsonButtons = "") : self
     {
         $postFieldSend = [
             'document' => curl_file_create($nameFile),
@@ -107,5 +119,4 @@ class Core
         curl_close( $objectCurl );
         return $this;
     }
-    
 }
